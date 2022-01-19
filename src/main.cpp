cb996e66b13676c2260e98de8abe873f4681680f
marcorobiso <95904043+marcorobiso@users.noreply.github.com>
2022-01-19 08:21:14 -0800

    Make  instruction  to the user

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {   std::string name;
+    std:: printf("NOTE: THIS PROGRAM IS CASE SENSITIVE, IT IS REQUIRED THAT YOU ENTER UPPERCASE LETTERS");
 
-    std::cout << " Please enter your name "<< std::endl;
+
+    std::cout << " Please enter your name: "<< std::endl;
     std::cin >> name;
 
-    
+    std::cout << ("HI ") << name;std::cout << (" ,Do you feel any of these symptoms? Type YES to confirm, otherwise NO.");
 
     return 0;
 }
